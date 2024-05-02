5f2437b2d0f5e278db376d9d3c936ff10b90566b
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2024-05-02 16:59:48 +0200

    Update Ecoutes.java

--- voxi0501/src/fr/voxi/admin/Ecoutes.java
+++ voxi0501/src/fr/voxi/admin/Ecoutes.java
@@ -1,5 +1,44 @@
 package fr.voxi.admin;
 
-public class Ecoutes {
+public class Oeuvres {
+	
+	private int numero;
+	private String titre;
+	private String auteur;
+	
+	public Oeuvres( int numero, String titre, String auteur) {
+		super();
+		this.numero = numero;
+		this.auteur = auteur;
+		this.titre = titre;
+	}
 
+	@Override
+	public String toString() {
+		return "Ecoutes [numero=" + numero + ", titre=" + titre + ", auteur=" + auteur + "]";
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public String getTitre() {
+		return titre;
+	}
+	
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
 }
