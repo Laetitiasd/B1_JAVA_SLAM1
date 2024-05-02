a2e0eaa0c5f9199cc0cec649acecf073a60de9b9
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2024-05-02 15:48:05 +0200

    Add files via upload

--- voxi0403/src/fr/voxi/admin/AppAdmin.java
+++ voxi0403/src/fr/voxi/admin/AppAdmin.java
@@ -18,14 +18,13 @@ public class AppAdmin {
 		System.out.println( "\n\n-----[Question 1]-----\n\n" ) ;
 		
 		/* Votre code pour la question 1 ici */
-
-		Candidature cdt1 = new Candidature(1, LocalDate.of(2024, 01, 11), "1");
-		
-		System.out.println("Numéro : "+cdt1.getNumero());
-		cdt1.EtatCandit(cdt1.getEtat()); 
-		System.out.println("Dépôt : " + cdt1.getDateDepot());
 	     
-	    
+		Candidature cdt1 = new Candidature(1, LocalDate.of(2024, 01, 11), "En Attente");
+        
+        System.out.println("Numéro : " + cdt1.getNumero());
+        System.out.println("État: " + cdt1.getEtat());
+        System.out.println("Dépôt : " + cdt1.getDateDepot());
+        
 	    
 		// 2- Tenter d'étudier la candidature 1
 		//		Si l'opération s'est déroulée avec succés, afficher "2- Candidature en cours d'étude"
@@ -36,10 +35,16 @@ public class AppAdmin {
 		
 		/* Votre code pour les questions 2 et 3 ici */
 		
-		
-		System.out.println("État de la candidature : " + cdt1.getEtat());
-		
-		
+
+		if (cdt1.getEtat().equals("En Attente")) {
+			cdt1.setEtat("Étudiée");
+            System.out.println("2- Candidature en cours d'étude");
+        } else {
+            System.out.println("2- La candidature ne peut pas être étudiée");
+        }
+    
+        System.out.println("État de la candidature : " + cdt1.getEtat());
+        
 		
 		// 4- Afficher l'état de la candidature
 		//		Si la candidature est en attente afficher "4- Candidature en attente."
@@ -47,28 +52,71 @@ public class AppAdmin {
 		//		Si la candidature est annulée "4- Candidature annulée."
 		//		Si ...
 		
+		System.out.println( "\n\n-----[Question 4]-----\n\n" ) ;
+		
 		/* Votre code pour la question 4 ici */
 		
-		
-		
-		
+		switch (cdt1.getEtat()) {
+        case "En Attente":
+            System.out.println("4- Candidature en attente.");
+            break;
+        case "Étudiée":
+            System.out.println("4- Candidature en cours d'étude.");
+            break;
+        case "Annulée":
+            System.out.println("4- Candidature annulée.");
+            break;
+        case "Acceptée":
+            System.out.println("4- Candidature acceptée.");
+            break;
+        case "Rejetée":
+            System.out.println("4- Candidature rejetée.");
+            break;
+        default:
+            System.out.println("4- État de la candidature non reconnu.");
+            break;
+				
+		}	
 		
 		// 5- Accepter puis afficher la candidature
 		
-		/* Votre code pour la question 5 ici */
+         System.out.println( "\n\n-----[Question 5]-----\n\n" ) ;
+            
+		/* Votre code pour la question 5 ici */ 
 		
-		
-		
-		
+            cdt1.setEtat("Acceptée");
+            System.out.println("5- Candidature acceptée.");
+            System.out.println(cdt1.toString()); 
 		
 		// 6- Afficher l'état de la candidature 
 		//		(voir question 4)
 		//		...
 
+            System.out.println( "\n\n-----[Question 6]-----\n\n" ) ;
 		/* Votre code pour la question 6 ici */
 		
 		
-		
+            switch (cdt1.getEtat()) {
+            case "En Attente":
+                System.out.println("4- Candidature en attente.");
+                break;
+            case "Étudiée":
+                System.out.println("4- Candidature en cours d'étude.");
+                break;
+            case "Annulée":
+                System.out.println("4- Candidature annulée.");
+                break;
+            case "Acceptée":
+                System.out.println("4- Candidature acceptée.");
+                break;
+            case "Rejetée":
+                System.out.println("4- Candidature rejetée.");
+                break;
+            default:
+                System.out.println("4- État de la candidature non reconnu.");
+                break;
+    				
+    		}	
 						
 		
 		// 7- Tenter d'annuler la candidature
@@ -76,9 +124,18 @@ public class AppAdmin {
 		//		Sinon afficher "7- Annulation refusée."
 		// 8- ... puis afficher la candidature
 		
+            System.out.println( "\n\n-----[Question 7 et 8 ]-----\n\n" ) ;
+            
 		/* Votre code pour les questions 7 et 8 ici */
 		
-		
+            cdt1.setEtat("En Attente");
+		if(cdt1.getEtat().equals("En Attente")) {
+			cdt1.setEtat("Annulée");
+			System.out.println("7- Candidature annulée");
+		} else {
+			System.out.println("7- Annulation refusée");
+		}
+		System.out.println("8- Etat" + " : " + cdt1.getEtat());
 		
 		
 		
@@ -87,9 +144,18 @@ public class AppAdmin {
 		//		Sinon afficher "9- Opération refusée."
 		// 10- ... puis afficher la candidature
 		
+		System.out.println( "\n\n-----[Question 9 et 10]-----\n\n" ) ;
+		
 		/* Votre code pour les questions 9 et 10 ici */
 		
-					
+		cdt1.setEtat("Étudiée");
+		if(cdt1.getEtat().equals("Étudiée")) {
+			cdt1.setEtat("Rejetée");
+			System.out.println("7- Candidature rejetée");
+		} else {
+			System.out.println("7- Opération refusée");
+		}
+		System.out.println("8- Etat" + " : " + cdt1.getEtat());		
 	
 	
 	
@@ -98,9 +164,22 @@ public class AppAdmin {
 		//		Sinon afficher "12- Opération refusée."
 		// 12- ... puis afficher la candidature
 		
+		System.out.println( "\n\n-----[Question 11 et 12]-----\n\n" ) ;
+		
 		/* Votre code pour les questions 11 et 12 ici */
 	
+		cdt1.setEtat("Étudiée");
+		if(cdt1.getEtat().equals("Étudiée")) {
+			cdt1.setEtat("Acceptée");
+			System.out.println("7- Candidature Acceptée");
+		} else {
+			System.out.println("7- Opération refusée");
+		}
+		System.out.println("8- Etat" + " : " + cdt1.getEtat());		
+	
+	
 
-	}
-
+		}
 }
+	
+
